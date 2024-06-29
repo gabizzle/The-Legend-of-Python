18116833e0b3c0fdde7128c69f18a7bc7f188aeb
Gabie <67624149+gabizzle@users.noreply.github.com>
2024-06-29 19:58:58 -0400

    Quadratic formula

--- Chapter 9.py
+++ Chapter 9.py
@@ -1,4 +1,4 @@
-# Chapter 9 – Pythagorean Theorem
+# Chapter 9 – Pythagorean Theorem & Quadratic Formula
 
 # Examples 
 # username = input('Enter your name: ')
@@ -12,9 +12,23 @@
 
 ################
 
-a = int(input('Enter the value of a: '))
-b = int(input('Enter the value of b: '))
+# a = int(input('Enter the value of a: '))
+# b = int(input('Enter the value of b: '))
 
-c = (a**2 + b**2)**0.5
+# c = (a**2 + b**2)**0.5
 
-print(c)
+# print(c)
+
+################
+
+# Quadratic Formula
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+root1 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+root2 = (-b - (b*b - 4*a*c)**0.5) / (2*a)
+
+print(root1)
+print(root2)
