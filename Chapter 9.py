f7977bd831ab1e7d6436579b636032fb3a7371c7
Gabie <67624149+gabizzle@users.noreply.github.com>
2024-06-29 19:57:10 -0400

    Pythagorean theorem is a^2 + b^2 = c^2

--- Chapter 9.py
+++ Chapter 9.py
@@ -1,11 +1,20 @@
 # Chapter 9 – Pythagorean Theorem
 
 # Examples 
-#username = input('Enter your name: ')
+# username = input('Enter your name: ')
 
-#print(username)
+    # print(username)
 
 ################
-age = int(input('What is your age? '))
+# age = int(input('What is your age? '))
+    
+    # print(age)
 
-print(age)
+################
+
+a = int(input('Enter the value of a: '))
+b = int(input('Enter the value of b: '))
+
+c = (a**2 + b**2)**0.5
+
+print(c)
