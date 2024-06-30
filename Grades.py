c230cb2dff2c9b1b77b34b14cda2f441f62f52e9
Gabie <67624149+gabizzle@users.noreply.github.com>
2024-06-30 12:22:11 -0400

    New files added

---
+++ Grades.py
@@ -0,0 +1,8 @@
+# Grades
+
+grade = int(input("Enter your grade: "))    # Ask the user to enter their grade and store it in the variable grade.
+
+if grade >= 55:             # If grade is greater than or equal to 55, then print "You passed."
+    print("You passed!")
+else:                        # Else, print "You failed."
+    print("You failed.")
