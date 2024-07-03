56cb1fd0ed87c8ea760bc6aa44ab8d3397634ce7
Gabie <67624149+gabizzle@users.noreply.github.com>
2024-07-03 00:47:54 -0400

    Added

--- Janken.py
+++ Janken.py
@@ -1,1 +1,22 @@
 # Janken / Rock Paper Scissors
+# Player vs Computer
+
+# Initialize
+
+
+
+print('-------------------------------')
+print('  Welcome to Janken じゃん拳!   ')
+print('   (Rock, Paper, Scissors)     ')
+print('-------------------------------')
+print('     1) Rock')
+print('     2) Paper')
+print('     3) Scissors\n')
+print('Enter your choice: ')
+
+choice = int(input())
+
+if choice == 1:
+    
+
+
